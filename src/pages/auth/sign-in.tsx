eec950bc65ec40ea73153e1b8473abe5b195e8f1
Matheus <matheus@matheusferreira.dev>
2024-09-25 23:32:36 -0300

    feat: add sign in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,76 @@
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
+  const {
+    formState: { isSubmitting },
+    register,
+    handleSubmit,
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (e) {
+      toast.error('Credenciais inválidas');
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
 
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+
+          <form
+            className="flex flex-col space-y-4"
+            onSubmit={handleSubmit(handleSignIn)}
+          >
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input
+                className="w-full"
+                id="email"
+                type="email"
+                {...register('email')}
+              />
+            </div>
+
+            <Button type="submit" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
