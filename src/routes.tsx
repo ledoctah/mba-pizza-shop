14b87e780b910caf01e0a112cf9c88e038e88c13
Matheus <matheus@matheusferreira.dev>
2024-09-28 12:28:25 -0300

    feat: add orders page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { AppLayout } from './pages/_layouts/app';
-import { AuthLayout } from './pages/_layouts/Auth';
-import { Dashboard } from './pages/app/Dashboard';
+import { AuthLayout } from './pages/_layouts/auth';
+import { Dashboard } from './pages/app/dashboard';
+import { Orders } from './pages/app/orders/orders';
 import { SignIn } from './pages/auth/sign-in';
 import { SignUp } from './pages/auth/sign-up';
 
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
