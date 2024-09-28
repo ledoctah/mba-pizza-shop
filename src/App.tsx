d5550f5036df11043d13c0c9014dc917a7fbff91
Matheus <matheus@matheusferreira.dev>
2024-09-28 16:59:04 -0300

    feat: configured http client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './global.css';
 
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'sonner';
 
+import { queryClient } from './lib/react-query';
 import { router } from './routes';
 import { ThemeProvider } from './theme/theme-provider';
 
@@ -13,7 +15,9 @@ export function App() {
       <ThemeProvider defaultTheme="system" storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider router={router}></RouterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}></RouterProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
