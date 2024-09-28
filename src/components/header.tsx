91d3c72b5cdc75acedcd6e4f45bfec464f476002
Matheus <matheus@matheusferreira.dev>
2024-09-28 12:06:56 -0300

    feat: add account menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Home, Pizza, UtensilsCrossed } from 'lucide-react';
 
 import { ThemeToggle } from '@/theme/theme-toggle';
 
+import { AccountMenu } from './account-menu';
 import { NavLink } from './nav-link';
 import { Separator } from './ui/separator';
 
@@ -27,6 +28,7 @@ export function Header() {
 
         <div className="ml-auto flex items-center gap-2">
           <ThemeToggle />
+          <AccountMenu />
         </div>
       </div>
     </div>
