3a7a53b37acbd4204830a2dfac681fc5c307e784
Matheus <matheus@matheusferreira.dev>
2024-09-28 12:47:18 -0300

    feat: add pagination component

---
+++ src/components/pagination.tsx
@@ -0,0 +1,70 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-react';
+
+import { Button } from './ui/button';
+
+export interface PaginationProps {
+  pageIndex: number;
+  totalCount: number;
+  perPage: number;
+}
+
+export function Pagination({
+  pageIndex,
+  perPage,
+  totalCount,
+}: PaginationProps) {
+  const pages = Math.ceil(totalCount / perPage) || 1;
+
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Button
+            aria-label="Primeira página"
+            variant="outline"
+            className="h-8 w-8 p-0"
+          >
+            <ChevronsLeft className="h-4 w-4"></ChevronsLeft>
+          </Button>
+
+          <Button
+            aria-label="Página anterior"
+            variant="outline"
+            className="h-8 w-8 p-0"
+          >
+            <ChevronLeft className="h-4 w-4"></ChevronLeft>
+          </Button>
+
+          <Button
+            aria-label="Próxima página"
+            variant="outline"
+            className="h-8 w-8 p-0"
+          >
+            <ChevronRight className="h-4 w-4"></ChevronRight>
+          </Button>
+
+          <Button
+            aria-label="Última página"
+            variant="outline"
+            className="h-8 w-8 p-0"
+          >
+            <ChevronsRight className="h-4 w-4"></ChevronsRight>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
