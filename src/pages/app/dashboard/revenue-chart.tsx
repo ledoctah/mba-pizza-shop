fe7f94104588619a76983aba4aebea507f7cfb44
Matheus <matheus@matheusferreira.dev>
2024-09-28 23:24:37 -0300

    feat: fetching revenue chart from api

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,3 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
+import { subDays } from 'date-fns';
+import { useMemo, useState } from 'react';
+import { DateRange } from 'react-day-picker';
 import {
   CartesianGrid,
   Line,
@@ -8,6 +12,7 @@ import {
 } from 'recharts';
 import colors from 'tailwindcss/colors';
 
+import { getDailyRevenueInPeriod } from '@/api/get-daily-revenue-in-period';
 import {
   Card,
   CardContent,
@@ -15,18 +20,28 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-
-const data = [
-  { date: '22/09', revenue: 1200 },
-  { date: '23/09', revenue: 875 },
-  { date: '24/09', revenue: 922 },
-  { date: '25/09', revenue: 476 },
-  { date: '26/09', revenue: 2367 },
-  { date: '27/09', revenue: 1298 },
-  { date: '28/09', revenue: 1453 },
-];
+import { DateRangePicker } from '@/components/ui/date-range-picker';
+import { Label } from '@/components/ui/label';
 
 export function RevenueChart() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  });
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryFn: () =>
+      getDailyRevenueInPeriod({ from: dateRange?.from, to: dateRange?.to }),
+    queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+  });
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((item) => ({
+      ...item,
+      receipt: item.receipt / 100,
+    }));
+  }, [dailyRevenueInPeriod]);
+
   return (
     <Card className="col-span-6">
       <CardHeader className="flex-row items-center justify-between pb-8">
@@ -36,36 +51,43 @@ export function RevenueChart() {
           </CardTitle>
           <CardDescription>Receita diária no período</CardDescription>
         </div>
+
+        <div className="flex items-center gap-3">
+          <Label>Período</Label>
+          <DateRangePicker onDateChange={setDateRange} date={dateRange} />
+        </div>
       </CardHeader>
 
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={data} style={{ fontSize: 12 }}>
-            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+        {dailyRevenueInPeriod && (
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={chartData} style={{ fontSize: 12 }}>
+              <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
 
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={80}
-              tickFormatter={(value: number) =>
-                value.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })
-              }
-            />
+              <YAxis
+                stroke="#888"
+                axisLine={false}
+                tickLine={false}
+                width={80}
+                tickFormatter={(value: number) =>
+                  value.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })
+                }
+              />
 
-            <CartesianGrid vertical={false} className="stroke-muted" />
+              <CartesianGrid vertical={false} className="stroke-muted" />
 
-            <Line
-              type="linear"
-              strokeWidth={2}
-              dataKey="revenue"
-              stroke={colors.violet[500]}
-            />
-          </LineChart>
-        </ResponsiveContainer>
+              <Line
+                type="linear"
+                strokeWidth={2}
+                dataKey="receipt"
+                stroke={colors.violet[500]}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   );
